f47412932a81fbc1813b652738ba690c3997c67d
znah54 <xcxcxc54@naver.com>
2024-04-16 09:17:53 +0900

    일단 저장

---
+++ day03/ex12_lambdas/ex15_pythons/Program.cs
@@ -0,0 +1,42 @@
+﻿// 파이썬용 라이브러리 사용등록
+using IronPython.Hosting;
+
+namespace ex15_pythons
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("파이썬 실행예제.");
+
+            var engine = Python.CreateEngine();
+            var scope = engine.CreateScope();
+            var paths = engine.GetSearchPaths();
+
+            // Python 경로 설정 @(리소스 키워드)
+            paths.Add(@"C:\DEV\Langs\Python311"); // 기본 파이썬 경로
+            paths.Add(@"C:\DEV\Langs\Python311\DLLs");
+            paths.Add(@"C:\DEV\Langs\Python311\Lib");
+            paths.Add(@"C:\DEV\Langs\Python311\Lib\site-packages");
+
+            paths.Add(@"C:\Users\user\AppData\Roaming\Python\Python311\site-packages");
+            paths.Add(@"C:\Users\user\AppData\Roaming\Python\Python311\site-packages\win32");
+            paths.Add(@"C:\Users\user\AppData\Roaming\Python\Python311\site-packages\win32\lib");
+
+            // 실행시킬 Python 파일 경로 설정
+            var filePath = @"C:\Sources\basic-csharp-2024\day03\cs03_basic_app\ex15_pythons\Test.py";
+            var source = engine.CreateScriptSourceFromFile(filePath);
+
+            // Python 실행
+            source.Execute(scope);
+
+            var PythonFunc = scope.GetVariable<Func<int, int, int>>("sum");
+            var result = PythonFunc(10, 7);
+            Console.WriteLine($"Python 함수실행 = {result}");
+
+            var PythonGreeting = scope.GetVariable<Func<string>>("sayGreeting");
+            var greeting = PythonGreeting();
+            Console.WriteLine($"결과 = {greeting}");
+        }
+    }
+}
---
+++ day03/ex12_lambdas/ex16_winforms/MainApp.cs
@@ -0,0 +1,34 @@
+﻿using static System.Net.Mime.MediaTypeNames;
+
+namespace ex16_winforms
+{
+    internal class MainApp : Form
+    {
+        static void Main(string[] args)
+        {
+            MainApp form = new MainApp(); // 새로 객체생성
+
+            form.Click += Form_Click;
+            form.KeyPress += Form_KeyPress;
+
+            Console.WriteLine("프로그램 시작");
+            Application.Run(form);
+
+            Console.WriteLine("프로그램 종료!");
+        }
+
+        // form KeyPress Eventhandler//
+        private static void Form_KeyPress(object? sender, KeyPressEventArgs e)
+        {
+            Console.WriteLine(e.KeyChar);
+        }
+
+
+        // 폼 클릭 이벤트핸들러.
+        private static void Form_Click(object? sender, EventArgs e)
+        {
+            Console.WriteLine("프로그램 종료 중...");
+            Application.Exit();
+        }
+    }
+}
